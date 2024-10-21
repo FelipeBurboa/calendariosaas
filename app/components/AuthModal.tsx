6e75fc7a2bcb553595cea290ab99f779383df3d2
Felipe Burboa <felipeburboa1991@gmail.com>
2024-10-21 05:52:53 -0300

    auth listo

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -9,17 +9,18 @@ import {
 } from "@/components/ui/dialog";
 import Logo from "@/public/logo.png";
 import Image from "next/image";
+import { signIn } from "../lib/auth";
+import { GoogleAuthButton, GighubAuthButton } from "./SubmitButtons";
 
 export function AuthModal() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="w-full justify-center">
-          Prueba Gratis
-        </Button>
+        <Button className="w-full justify-center">Prueba Gratis</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[360px]">
         <DialogHeader className="flex flex-row items-center gap-2 justify-center">
+          <DialogTitle></DialogTitle>
           <Image src={Logo} alt="logo" className="size-10" />
           <h4 className="text-3xl font-bold">
             <span className="bg-gradient-to-r from-red-500 via-orange-300 to-green-400 bg-[length:90%_4px] bg-no-repeat bg-bottom">
@@ -30,6 +31,30 @@ export function AuthModal() {
             </span>
           </h4>
         </DialogHeader>
+        <DialogDescription>
+          <p className="text-center">
+            <span className="font-semibold">Prueba gratis</span> para obtener
+            acceso a todas las funcionalidades del sistema
+          </p>
+        </DialogDescription>
+        <div className="flex flex-col gap-2 w-full px-7">
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <GoogleAuthButton />
+          </form>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            <GighubAuthButton />
+          </form>
+        </div>
       </DialogContent>
     </Dialog>
   );
---
+++ app/components/SubmitButtons.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
+import GoogleLogo from "@/public/google.svg";
+import GithubLogo from "@/public/github.svg";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+
+export function GoogleAuthButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled variant="outline" className="w-full">
+          <Loader2 className="size-4 mr-2 animate-spin" />
+          Cargando...
+        </Button>
+      ) : (
+        <Button variant="outline" className="w-full">
+          <Image src={GoogleLogo} alt="Google Logo" className="size-4 mr-2" />
+          Crear cuenta con Google
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function GighubAuthButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled variant="outline" className="w-full">
+          <Loader2 className="size-4 mr-2 animate-spin" />
+          Cargando...
+        </Button>
+      ) : (
+        <Button variant="outline" className="w-full">
+          <Image src={GithubLogo} alt="Github Logo" className="size-4 mr-2" />
+          Crear cuenta con Github
+        </Button>
+      )}
+    </>
+  );
+}
